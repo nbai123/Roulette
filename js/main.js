ef8a049ca69780658da0aea6ff7a5fe7e3837b93
Nick Bai <nick@nick.com>
2019-08-05 09:32:18 -0700

    first commit create html css js readme

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/ 
+let board, player
+
+
+/*----- app's state (variables) -----*/ 
+
+
+
+/*----- cached element references -----*/ 
+
+
+
+/*----- event listeners -----*/ 
+
+
+
+/*----- functions -----*/
+
+
