b5bca85de42ccb8645729fe4ec45b4301e2a1f2e
Nick Bai <nick@nick.com>
2019-08-05 10:46:47 -0700

    add basic board basic events and pseudo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /*----- constants -----*/ 
 let board, player
 
+//let wheelNum = .random value from 00 0 1-36
+//returned value = winningNum
+
+//let winningNum = result from random wheelNum
+
 
 /*----- app's state (variables) -----*/ 
 
@@ -11,9 +16,20 @@ let board, player
 
 
 /*----- event listeners -----*/ 
-
-
+document.querySelector('section.board').addEventListener('click', bet);
+//on click input value of bet selected
+//on click select 1-18, 19-36, even, odds, red, black, 1st 12, 2nd 12, last 12
+//on click reset button reinitialize the board
+document.querySelector('section.board')
+//on click initialize timer after 10 seconds no more bets are allowed.. wait 3 more seconds and show wheelNum
+document.querySelector('div.chips').addEventListener('click', value);
+//on click selects the chip value selected
 
 /*----- functions -----*/
+function bet (){
+    
+}
 
+function value (){
 
+}
